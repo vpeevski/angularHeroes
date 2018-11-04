d5580f45f393aafb8d406434d83576777666b61d
vpeevski <vpeevski@vmware.com>
2018-11-04 09:56:34 +0200

    Autofocus directive

---
+++ src/app/directives/autofocus.directive.ts
@@ -0,0 +1,11 @@
+import { Directive,Input, ElementRef, Renderer } from '@angular/core';
+
+@Directive({
+  selector: '[autofocus]'
+})
+export class AutofocusDirective {
+  constructor(private elementRef: ElementRef) {}
+  ngOnInit(){
+    this.elementRef.nativeElement.focus();
+  }
+}
